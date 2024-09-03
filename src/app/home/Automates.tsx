70bc7d2130be0eded40d1ac5eb12be02844b570d
itd3vbox <it.d3vbox@gmail.com>
2024-09-03 23:46:00 +0200

    home page - tasks & automates ready

--- src/app/home/Automates.tsx
+++ src/app/home/Automates.tsx
@@ -15,7 +15,7 @@ interface AutomatesProps
 
 interface AutomatesState
 {
-    
+    data: any
 }
 
 export default class Automates extends React.Component<any, AutomatesState>
@@ -25,30 +25,122 @@ export default class Automates extends React.Component<any, AutomatesState>
     {
         super(props)
         this.state = {
-
+            data: null,
         }
+    }
+
+    
+    componentDidMount(): void 
+    {
+        this.search()
+    }
+
+    async search()
+    {
+        const formData = {
+            is_asc: false,
+            max: 2,
+        }
+    
+        const url: string = 'http://projectmanager.demo/api/automates/search'
+
+        const token = localStorage.getItem('access_token')
+        
+        if (!token) {
+            console.error('No token found')
+            return
+        }
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                    'Origin': 'http://localhost:3000',
+                },
+                body: JSON.stringify(formData)
+            })
+
+            const result = await response.json()
+            console.log(result)
+            this.setState({
+                data: result.data,
+            })
+        } 
+        catch (error) 
+        {
+            console.error('Error fetch', error)
+        }
+    }
+
+    async execute(data: any)
+    {
+        const formData = new FormData()
+        //formData.append('_method', 'PATCH')
+        formData.append('status', String(data.status === 1 ? 2 : 1))
+    
+        const url: string = 'http://projectmanager.demo/api/automates/' + data.id + '/execute'
+
+        const token = localStorage.getItem('access_token')
+        
+        if (!token) {
+            console.error('No token found')
+            return
+        }
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                    'Origin': 'http://localhost:3000',
+                },
+                body: formData,
+            })
+
+            const result = await response.json()
+            console.log(result)
+            this.search()
+        } 
+        catch (error) 
+        {
+            console.error('Error fetch', error)
+        }
+    }
+
+    handleOnExecute(data: any)
+    {
+        this.execute(data)
     }
 
     renderAutomates()
     {
-        const tests: any = []
+        const automates: any = []
+        const automatesData: any = this.state.data ? this.state.data.data : []
 
-        for (let index = 0; index < 5; index++) 
+        for (let index = 0; index < automatesData.length && index < 5; index++) 
         {
-            tests.push(
+            const automate = automatesData[index]
+            automates.push(
                 <div className={ "automate" + (index === 0 ? ' on-progress' : '') } key={ index }>
                     <div className="icon">
                         <ClockIcon />
                     </div>
-                    <div className="title">Debug Project 1</div>
-                    <div className="duration">25m 20s</div>
-                    <button type="button">
+                    <div className="name">{ automate.name }</div>
+                    <div className="duration">{ automate.duration_info.value_text }</div>
+                    <button type="button"
+                        onClick={ () => this.handleOnExecute(automate) }>
                         <PlayIcon />
                     </button>
                 </div>
             )
         }
-        return tests
+        return automates
     }
 
     render()
--- src/app/home/Tasks.tsx
+++ src/app/home/Tasks.tsx
@@ -4,6 +4,7 @@ import React from "react"
 import {
     PlayIcon,
     CheckCircleIcon,
+    CheckIcon,
 } from '@heroicons/react/24/outline';
 
 
@@ -15,7 +16,7 @@ interface TasksProps
 
 interface TasksState
 {
-    
+    data: any
 }
 
 export default class Tasks extends React.Component<any, TasksState>
@@ -25,22 +26,113 @@ export default class Tasks extends React.Component<any, TasksState>
     {
         super(props)
         this.state = {
-
+            data: null,
         }
+    }
+
+    componentDidMount(): void {
+        this.search()
+    }
+
+    async search()
+    {
+        const formData = {
+            is_asc: false,
+            max: 2,
+        }
+    
+        const url: string = 'http://projectmanager.demo/api/tasks/search'
+
+        const token = localStorage.getItem('access_token')
+        
+        if (!token) {
+            console.error('No token found')
+            return
+        }
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                    'Origin': 'http://localhost:3000',
+                },
+                body: JSON.stringify(formData)
+            })
+
+            const result = await response.json()
+            console.log(result)
+            this.setState({
+                data: result.data,
+            })
+        } 
+        catch (error) 
+        {
+            console.error('Error fetch', error)
+        }
+    }
+
+    async updateStatus(data: any)
+    {
+        const formData = new FormData()
+        formData.append('_method', 'PATCH')
+        formData.append('status', String(data.status === 1 ? 2 : 1))
+    
+        const url: string = 'http://projectmanager.demo/api/tasks/' + data.id + '/status'
+
+        const token = localStorage.getItem('access_token')
+        
+        if (!token) {
+            console.error('No token found')
+            return
+        }
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                    'Origin': 'http://localhost:3000',
+                },
+                body: formData,
+            })
+
+            const result = await response.json()
+            console.log(result)
+            this.search()
+        } 
+        catch (error) 
+        {
+            console.error('Error fetch', error)
+        }
+    }
+
+    handleStatusOnUpdate(data: any)
+    {
+        this.updateStatus(data)
     }
 
     renderTasks()
     {
         const tasks: any = []
+        const tasksData: any = this.state.data ? this.state.data.data : []
 
-        for (let index = 0; index < 5; index++) 
+        for (let index = 0; index < tasksData.length && index < 5; index++) 
         {
+            const task = tasksData[index]
+            console.log(task)
             tasks.push(
                 <div className="task" key={ index }>
                     <div className="number">{ index + 1 }</div>
-                    <div className="title">Debug Project 1</div>
-                    <button type="button">
-                        <CheckCircleIcon />                       
+                    <div className="title">{ task.title }</div>
+                    <button type="button" onClick={ () => this.handleStatusOnUpdate(task) }>
+                    { task.status === 1 && (<CheckCircleIcon />) }
+                    { task.status !== 1 && (<CheckIcon />) }                           
                     </button>
                 </div>
             )
